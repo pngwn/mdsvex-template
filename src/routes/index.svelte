2a4cd681e74e0bd8eb2003ab397e437c32ba7bf6
pngwn <hello@pngwn.io>
2021-05-31 15:08:15 +0100

    setup mdsvex

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,4 +1,25 @@
+<script>
+	// get an array of all routes
+	const routes = import.meta.globEager("./blog/*.svx");
+
+	// make them more usable by grabbing the title from frontmatter and the href
+	const links = Object.entries(routes).map(([name, { metadata }]) => ({
+		href: name.replace(".svx", ""),
+		title: metadata.title,
+	}));
+
+	console.log(links);
+</script>
+
 <h1>Welcome to SvelteKit</h1>
 <p>
 	Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation
 </p>
+
+<h2>Blog</h2>
+
+<ul>
+	{#each links as { href, title }}
+		<li><a {href}>{title}</a></li>
+	{/each}
+</ul>
--- svelte.config.js
+++ svelte.config.js
@@ -1,9 +1,13 @@
+import { mdsvex } from 'mdsvex'
+
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
 		// hydrate the <div id="svelte"> element in src/app.html
 		target: '#svelte'
 	},
+	extensions: ['.svelte', '.svx'],
+	preprocess: mdsvex()
 };
 
 export default config;
